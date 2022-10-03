576015fb400d08d55009c5f1bd645ed778b5aaa0
Will W <will.winston@gmail.com>
2022-10-03 12:46:28 -0400

    Added Port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const shortid = require('shortid');
 
 const express = require('express');
 const path = require('path');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
